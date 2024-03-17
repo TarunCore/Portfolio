cc26679830837f7e401b0aea2b74677c4f45a230
TarunCore <112856864+TarunCore@users.noreply.github.com>
2024-03-17 13:58:59 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='text-white'>
+      <h1 className="text-3xl font-bold underline text">
+      Hello world!
+    </h1>
+    </div>
+  )
+}
+
+export default App
